54163098ab29033bbd7aa1411f9d61e082cee448
chiu <117958646+chiuuung@users.noreply.github.com>
2023-02-15 01:41:01 +0800

    Update send data back to server python code.py

--- send data back to server python code.py
+++ send data back to server python code.py
@@ -1,72 +1,43 @@
-#Python script to read serial port data and send them to UBIDOTS cloud
+#Python script to read serial port data from arduino and send them to srever
+
 import time
 import requests
-import math
-import random
 import serial
-TOKEN = "BBFF-ietNSqNZWKDcl7MsoIgp7YKYORLz9E"  # Put your TOKEN here
-DEVICE_LABEL = "My_PC"  # Put your device label here 
-VARIABLE_LABEL_1 = "student number"  # Put your first variable label here
 
 
-#configure serial port
-arduino = serial.Serial('COM3', 9600, timeout=1)
+arduino = serial.Serial('COM3', 9600, timeout=1)  #configure serial port
 print("Reading data from serial port.....");
 time.sleep(2)
-arduino.reset_input_buffer() # Delete any stale data.
+arduino.reset_input_buffer() # Delete stale data
 
-def build_payload(variable_1, data):
-    # Creates two random values for sending data
-    value_1 = int(data)
-    payload = {variable_1: value_1}
+
+def build_payload(data):
+    payload = {"student_id": int(data)} # sending data
     return payload
 
 
-def post_request(payload):
-    # Creates the headers for the HTTP requests
-    url = "http://industrial.api.ubidots.com"
-    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
-    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
-
-    # Makes the HTTP requests
-    status = 400
-    attempts = 0
-    while status >= 400 and attempts <= 5:
-        req = requests.post(url=url, headers=headers, json=payload)
-        status = req.status_code
-        attempts += 1
-        time.sleep(1)
-
-    # Processes results
-    #print(req.status_code, req.json())
-    if status >= 400:
-        print("[ERROR] Could not send data after 5 attempts, please check \
-            your token credentials and internet connection")
-        return False
-
-    print("Device ID updated...wait for data transfer")
+def post_request(payload): # Creates the headers for the HTTP requests
+    headers = {"Content-Type": "application/json"}
+    req = requests.post("https://api.ccit4080.tylerl.cyou/attendance/checkin", headers=headers, json=payload)
+    status = req.status_code
+    print("Waitting for data transfer...")
+    print(".")
+    print("..")
+    print("...")
     return True
 
 
-def main():
-# Reading all bytes available bytes till EOL
-    line = arduino.readline() 
+def main(): 
+    line = arduino.readline() # Reading all bytes available bytes till EOL
     if line:
-        # Converting Byte Strings into unicode strings
-        rxdata = line.decode()  
-        # Converting Unicode String into integer
-        #num = int(rxdata) 
-        #print(num)
-        tmp = rxdata
-        
-        #send collected data to cloud as a payload
-        payload = build_payload(VARIABLE_LABEL_1,tmp)
-        print(tmp)
-        del tmp #clear received data
-
+        rxdata = line.decode()   # Converting Byte Strings into unicode strings
+        sid = rxdata
+        payload = build_payload(sid) #send collected data to cloud as a payload
+        print(sid)
+        del sid #clear received data
         print("Data transfer started")
         post_request(payload)
-        print("Data Successfully sent to cloud")
+        print("Data Successfully sent to server")
         print("\nReading data from serial port.....");
 
 
